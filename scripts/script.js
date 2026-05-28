7f17530f5260266316d88bcef06a9a0cebb99807
DevScissors <sean.scissors@gmail.com>
2026-05-27 18:01:41 -0700

    Initial setup of the game working in the console when calling the playRound through the gameController and choosing the row/column to place the value in

---
+++ scripts/script.js
@@ -0,0 +1,104 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const placeSelection = (row, column, player) => {
+        const availableCells = board
+            .filter((row) => row[column].getValue() === 0)
+            .map((row) => row[column]);
+
+        if (!availableCells.length) return console.log("Square has already been selected");
+
+        board[row][column].playerChoice(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) =>
+            row.map((cell) => cell.getValue())
+        );
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, placeSelection, printBoard };
+}
+
+function Cell() {
+    let value = 0;
+
+    const playerChoice = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        playerChoice,
+        getValue
+    };
+}
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 'X',
+        },
+        {
+            name: playerTwoName,
+            token: 'O',
+        },
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(
+            `${getActivePlayer().name}'s chooses to play ${getActivePlayer().token} into [${row}, ${column}]`
+        );
+        board.placeSelection(row, column, getActivePlayer().token);
+
+        /*  This is where we would check for a winner and handle that logic,
+              such as a win message. */
+
+        // Switch player turn
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    // Initial play game message
+    printNewRound();
+
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+        playRound,
+        getActivePlayer,
+    };
+}
+
+const game = GameController();
